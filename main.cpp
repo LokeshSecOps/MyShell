81b79a5392e737df2330c660d0fe3e956dae2ba1
LokeshSecOps <Lokeshsharma02110@gmail.com>
2025-07-01 00:15:41 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 #include <map>
 
 enum JobState { Running, Stopped };
-
 struct Job {
     pid_t pid;
     std::string command;
@@ -22,20 +21,7 @@ struct Job {
 std::vector<std::string> history;
 std::map<pid_t, Job> jobs;
 pid_t foregroundPid = 0;
-
-void handle_SIGTSTP(int sig) {
-    if (foregroundPid > 0) {
-        kill(foregroundPid, SIGTSTP);
-    }
-}
-
-void handle_SIGCHLD(int sig) {
-    int status;
-    pid_t pid;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        jobs.erase(pid);
-    }
-}
+pid_t shellPid;
 
 std::vector<std::string> split(const std::string& str, char delimiter) {
     std::vector<std::string> parts;
@@ -47,73 +33,75 @@ std::vector<std::string> split(const std::string& str, char delimiter) {
     return parts;
 }
 
+void handle_SIGTSTP(int sig) {
+    if (foregroundPid > 0) {
+        kill(foregroundPid, SIGTSTP);
+    } else {
+        std::cout << "\n[MyShell] No foreground job to suspend.\n";
+        std::cout.flush();
+    }
+}
+
+void handle_SIGCHLD(int sig) {
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        if (jobs.count(pid)) {
+            std::cout << "\n[Done PID: " << pid << "] " << jobs[pid].command << std::endl;
+            jobs.erase(pid);
+            std::cout << "[MyShell:" << getcwd(nullptr, 0) << "]$ ";
+            std::cout.flush();
+        }
+    }
+}
+
 void execute_piped_commands(const std::vector<std::string>& commands, bool runInBackground, const std::string& fullCmd) {
     int n = commands.size();
     int pipefd[2], in_fd = 0;
-
     pid_t lastPid = -1;
+
     for (int i = 0; i < n; ++i) {
         pipe(pipefd);
-
         std::string inputFile, outputFile;
         bool append = false;
         std::vector<char*> args;
-
         std::istringstream iss(commands[i]);
         std::string token;
         while (iss >> token) {
-            if (token == "<") {
-                iss >> inputFile;
-            } else if (token == ">") {
-                iss >> outputFile;
-                append = false;
-            } else if (token == ">>") {
-                iss >> outputFile;
-                append = true;
-            } else {
-                args.push_back(strdup(token.c_str()));
-            }
+            if (token == "<") iss >> inputFile;
+            else if (token == ">") { iss >> outputFile; append = false; }
+            else if (token == ">>") { iss >> outputFile; append = true; }
+            else args.push_back(strdup(token.c_str()));
         }
         args.push_back(nullptr);
 
         pid_t pid = fork();
         if (pid == 0) {
             setpgid(0, 0);
-
             if (i > 0) {
                 dup2(in_fd, STDIN_FILENO);
                 close(in_fd);
             }
-
             if (i < n - 1) {
                 dup2(pipefd[1], STDOUT_FILENO);
                 close(pipefd[1]);
             }
-
             if (!inputFile.empty()) {
                 int fd = open(inputFile.c_str(), O_RDONLY);
-                if (fd < 0) {
-                    perror("Input file error");
-                    exit(EXIT_FAILURE);
-                }
+                if (fd < 0) { perror("Input error"); exit(1); }
                 dup2(fd, STDIN_FILENO);
                 close(fd);
             }
-
             if (!outputFile.empty() && i == n - 1) {
                 int fd = open(outputFile.c_str(), O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC), 0644);
-                if (fd < 0) {
-                    perror("Output file error");
-                    exit(EXIT_FAILURE);
-                }
+                if (fd < 0) { perror("Output error"); exit(1); }
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
             }
-
             close(pipefd[0]);
             execvp(args[0], args.data());
-            perror("Command failed");
-            exit(EXIT_FAILURE);
+            perror("execvp failed");
+            exit(1);
         } else if (pid > 0) {
             setpgid(pid, pid);
             if (i == n - 1) lastPid = pid;
@@ -122,7 +110,6 @@ void execute_piped_commands(const std::vector<std::string>& commands, bool runIn
         }
 
         for (char* arg : args) free(arg);
-
         close(pipefd[1]);
         if (i > 0) close(in_fd);
         in_fd = pipefd[0];
@@ -131,14 +118,29 @@ void execute_piped_commands(const std::vector<std::string>& commands, bool runIn
     if (runInBackground) {
         jobs[lastPid] = {lastPid, fullCmd, Running};
         std::cout << "[Background PID: " << lastPid << "]" << std::endl;
+        tcsetpgrp(STDIN_FILENO, shellPid);
     } else {
         foregroundPid = lastPid;
-        waitpid(lastPid, nullptr, 0);
+        tcsetpgrp(STDIN_FILENO, lastPid);
+        int status;
+        waitpid(lastPid, &status, WUNTRACED);
+        tcsetpgrp(STDIN_FILENO, shellPid);
+        if (WIFSTOPPED(status)) {
+            jobs[lastPid] = {lastPid, fullCmd, Stopped};
+        } else {
+            jobs.erase(lastPid);
+        }
         foregroundPid = 0;
     }
 }
 
 int main() {
+    shellPid = getpid();
+    setpgid(shellPid, shellPid);
+    tcsetpgrp(STDIN_FILENO, shellPid);
+
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
     signal(SIGTSTP, handle_SIGTSTP);
     signal(SIGCHLD, handle_SIGCHLD);
 
@@ -149,12 +151,16 @@ int main() {
         std::cout.flush();
 
         std::string input;
-        std::getline(std::cin, input);
+        if (!std::getline(std::cin, input)) {
+            std::cout << "\n[MyShell] Input stream closed. Exiting.\n";
+            break;
+        }
+
+        input.erase(input.find_last_not_of(" \t\n\r\f\v") + 1);
         if (input.empty()) continue;
         history.push_back(input);
 
         bool runInBackground = false;
-        input.erase(input.find_last_not_of(" \t\n\r\f\v") + 1);
         if (!input.empty() && input.back() == '&') {
             runInBackground = true;
             input.pop_back();
@@ -183,21 +189,33 @@ int main() {
         }
 
         if (input == "fg") {
-            bool found = false;
-            for (auto& [pid, job] : jobs) {
-                if (job.state == Stopped || job.state == Running) {
-                    foregroundPid = pid;
-                    job.state = Running;
-                    kill(pid, SIGCONT);
-                    waitpid(pid, nullptr, 0);
-                    jobs.erase(pid);
-                    found = true;
-                    break;
-                }
+    bool found = false;
+    for (auto& [pid, job] : jobs) {
+        if (job.state == Stopped || job.state == Running) {
+            foregroundPid = pid;
+            job.state = Running;
+            tcsetpgrp(STDIN_FILENO, pid);
+            kill(pid, SIGCONT);
+
+            int status;
+            waitpid(pid, &status, WUNTRACED);
+
+            if (WIFSTOPPED(status)) {
+                job.state = Stopped; 
+            } else {
+                jobs.erase(pid); 
             }
-            if (!found) std::cout << "No stopped/running jobs to bring to foreground.\n";
-            continue;
+
+            tcsetpgrp(STDIN_FILENO, shellPid);
+            foregroundPid = 0;
+            found = true;
+            break;
         }
+    }
+    if (!found) std::cout << "No job to bring to foreground.\n";
+    continue;
+}
+
 
         if (input == "bg") {
             bool found = false;
@@ -210,7 +228,7 @@ int main() {
                     break;
                 }
             }
-            if (!found) std::cout << "No stopped jobs to resume in background.\n";
+            if (!found) std::cout << "No stopped job found.\n";
             continue;
         }
 
@@ -223,7 +241,6 @@ int main() {
             continue;
         }
 
-        // Handle pipes
         std::vector<std::string> commands = split(input, '|');
         execute_piped_commands(commands, runInBackground, input);
     }
